a993025b337e88093d956bd845507221bcbc0101
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2026-02-03 19:35:52 +0530

    Rc.8

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "AstroX~FMAx0YAb#3UA4molr5U0T5MDqQMYhyy017i2HVcjrhtb9xH6GTMU",,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://ameen-api.vercel.app/AstroX",
 ALIVE_MSG: process.env.ALIVE_MSG || " *AstroX Was Alive Now💗* ",
 OWNER_NAME: process.env.OWNER_NAME || "Ameen Sulthan",
